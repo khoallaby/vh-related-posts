4a710c58ed9bd8a03dd30c839099b3ba49766c85
Andy Nguyen <andrew.nguyen@colorado.edu>
2014-06-02 21:03:27 -0600

    fix/add lightbox links to more, thumbnails. pp lightbox paragraph fix

--- templates/box-general.php
+++ templates/box-general.php
@@ -2,8 +2,13 @@
     <h2><?php _e( $h2_titles[$type], 'vh-related-posts'); ?></h2>
     <div class="featured-image">
     <?php
-        if ( has_post_thumbnail($box->ID))
+        if ( has_post_thumbnail($box->ID)) {
+            $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($box->ID), 'large');
+            #echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" class="lightbox">';
+            echo '<a href="' . get_permalink($box->ID) . '">';
             echo get_the_post_thumbnail($box->ID, 'vh-rp-image');
+            echo '</a>';
+        }
     ?>
     </div>
     <h3><?php echo esc_html($box->post_title); ?></h3>
